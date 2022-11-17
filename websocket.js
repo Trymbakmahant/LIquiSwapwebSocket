9cfa83a1d7cd7f9af49504d28334fe08abf17637
Trymbak Mahant <trymbak0102mahant@gmail.com>
2022-11-17 12:10:17 +0530

    new version of websocket

--- websocket.js
+++ websocket.js
@@ -3,53 +3,52 @@ const { ethers } = require("ethers");
 const { ABI } = require("./abi");
 const express = require("express");
 const app = express();
-const main = async () => {
-  const contractAddress = "0x64D1eE237c1633044b812F0a618a9171D7d2A803";
+const contractAddress = "0x64D1eE237c1633044b812F0a618a9171D7d2A803";
 
-  const provider = new ethers.providers.WebSocketProvider(
-    "wss://polygon-mumbai.g.alchemy.com/v2/KM1Kv-cqY7LlaPsoximQwOASxTzExuR5"
-  );
+const provider = new ethers.providers.WebSocketProvider(
+  "wss://polygon-mumbai.g.alchemy.com/v2/KM1Kv-cqY7LlaPsoximQwOASxTzExuR5"
+);
 
-  const contract = new ethers.Contract(contractAddress, ABI, provider);
+const contract = new ethers.Contract(contractAddress, ABI, provider);
 
-  const _id = Math.random() * 100000000;
-  contract.on("Liquidation", async (user, price, amountMATIC, amountDAI) => {
-    const date = new Date(Date.now());
-    const time =
-      date.toLocaleString("en-GB", { timeZone: "Europe/London" }) +
-      " (UTC + 0)";
-    const address = user.toLowerCase();
-    const sender = contractAddress;
-    const info = {
-      _id,
-      address,
-      sender,
-      price: ethers.utils.formatEther(price.toString()),
-      receiver: address,
-      amountMATIC: ethers.utils.formatEther(amountMATIC.toString()),
-      amount: ethers.utils.formatEther(amountDAI.toString()),
-      time,
-      method: "Liquidate",
-      token: "DAI",
-    };
-    console.log(info);
-    try {
-      const result = await axios.post(
-        "https://liqui.onrender.com/api/ipfs",
-        info
-      );
-      console.log(result.data);
-    } catch (err) {
-      console.log(err);
-    }
+contract.on("Liquidation", async (user, price, amountMATIC, amountDAI) => {
+  const date = new Date();
+  const time =
+    date.toLocaleString("en-GB", { timeZone: "Europe/London" }) + " (UTC + 0)";
 
-    console.log(result);
-  });
-};
+  const address = user.toLowerCase();
+  const sender = contractAddress;
+  const info = {
+    address,
+    sender,
+    price: ethers.utils.formatEther(price.toString()),
+    receiver: address,
+    amountMATIC: ethers.utils.formatEther(amountMATIC.toString()),
+    amount: ethers.utils.formatEther(amountDAI.toString()),
+    time,
+    method: "Liquidate",
+    token: "DAI",
+  };
+  console.log(info);
 
-main()
-  .then()
-  .catch((e) => console.log(e));
+  const result = await axios.post("https://liqui.onrender.com/api/ipfs", info);
+
+  console.log(result);
+});
+
+contract.on("Deposit", (user, token, amount, balance) => {
+  const data = {
+    user,
+    token,
+    amount,
+    balance,
+  };
+
+  console.log(JSON.stringify(data));
+});
 
 const port = 8081;
-app.listen(port, console.log(`Listening on port ${port}...`));
+app.listen(
+  process.env.PORT || port,
+  console.log(`Listening on port ${port}...`)
+);
